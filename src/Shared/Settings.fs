8a7746369ba2dfb0b6594bb85a72d62bdfbe409b
Robin Munn <rmunn@pobox.com>
2019-08-27 14:19:43 +0700

    Can now get default values from environment
    If the environment variable is not set, we still specify a fallback
    default so we'll never be returning null.

--- src/Shared/Settings.fs
+++ src/Shared/Settings.fs
@@ -4,12 +4,26 @@ module Settings =
     let defaultValue (def : 'a) (value : 'a) =
         if value = Unchecked.defaultof<'a> then def else value
 
+    let defaultEnv (env : string) (def : string) (value : string) =
+#if FABLE_COMPILER
+        // Javascript can't access environment variables
+        defaultValue def value
+#else
+        if isNull value then
+            let result = System.Environment.GetEnvironmentVariable env
+            if System.String.IsNullOrEmpty result then def else result
+        else
+            value
+#endif
+
     [<CLIMutable>]
     type AudioSettings = {
         FfmpegPath : string
         Development : string
+        Path : string
     } with
         member this.FixDefault() =
             { this with
                 FfmpegPath = this.FfmpegPath |> defaultValue "default path"
-                Development = this.Development |> defaultValue "default dev" }
+                Development = this.Development |> defaultValue "default dev"
+                Path = this.Path |> defaultEnv "PATH" "default path" }
