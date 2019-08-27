b449b07c39a86e285b42b9f4d17dc10135afad55
Robin Munn <rmunn@pobox.com>
2019-08-27 16:06:10 +0700

    Move settings helper functions to own file
    Cleaner Settings.fs file as a result.

--- src/Shared/Settings.fs
+++ src/Shared/Settings.fs
@@ -1,31 +1,7 @@
 namespace Shared
 
 module Settings =
-    let defaultValue (def : 'a) (value : 'a) =
-        if value = Unchecked.defaultof<'a> then def else value
-
-    let defaultEnv (env : string) (def : string) (value : string) =
-#if FABLE_COMPILER
-        // Javascript can't access environment variables
-        defaultValue def value
-#else
-        if isNull value then
-            let result = System.Environment.GetEnvironmentVariable env
-            if System.String.IsNullOrEmpty result then def else result
-        else
-            value
-#endif
-
-    let defaultEnvParsed (parse : string -> 'a) (env : string) (def : 'a) (value : 'a) =
-#if FABLE_COMPILER
-        defaultValue def value
-#else
-        if value = Unchecked.defaultof<'a> then
-            let result = System.Environment.GetEnvironmentVariable env
-            if System.String.IsNullOrEmpty result then def else parse result
-        else
-            value
-#endif
+    open DefaultHelpers
 
     [<CLIMutable>]
     type MySqlSettings = {
