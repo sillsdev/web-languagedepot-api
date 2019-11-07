4571fbfa752545ee8899b1eed71f2e1f820a3db5
Robin Munn <rmunn@pobox.com>
2019-11-07 16:49:46 +0700

    WIP: Rewrite shared model almost completely
    No changes have been made yet to either client or server, so of course
    this won't compile.

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -19,15 +19,6 @@ type SharedProjects = {
     Projects : string list
 }
 
-type LoginInfo = {
-    username : string
-    password : string
-}
-
-type PatchProjects = {
-    addUser : SharedUser option
-    removeUser : SharedUser option
-}
 
 type ProjectType =
     | Unknown
@@ -39,48 +30,228 @@ type ProjectType =
     | AdaptIt
     | School
 
+type RoleType =
+    | Manager
+    | Contributor
+    | Observer
+    | Programmer
+    with
+        member _.ToString =
+            function
+            | Manager -> "Manager"
+            | Contributor -> "Contributor"
+            | Observer -> "Observer"  // TODO: Switch back to the "do not use" text if it turns out we still want to disallow the Observer role
+            | Programmer -> "Programmer"
+        member _.TryOfString (s : string) =
+           match s.ToLowerInvariant() with
+            | "manager" -> Some Manager
+            | "contributer" -> Some Contributor  // Misspelling of contributor, but this is how it's spelled in Redmine so we have to deal with it
+            | "contributor" -> Some Contributor
+            | "member" -> Some Contributor
+            | "languagedepotprogrammer" -> Some Programmer
+            | "programmer" -> Some Programmer
+            | "obv - do not use" -> Some Observer
+            | "observer" -> Some Observer
+            | _ -> None
+        member this.OfString s =
+            match this.TryOfString s with
+            | Some value -> value
+            | None -> failwith (sprintf "Unknown role type %s" s)
+
+// TODO: Decide how to convert between role names in Redmine and the role types (Manager, etc) that our future model will use.
+// Right now we just hardcode strings since they're not likely to change anytime soon.
+
+module Dto =
+    type UserDetails = {
+        username : string
+        firstName : string
+        lastName : string
+        emailAddresses : string list // (default first, rest in whatever order they come back from the database)
+        // (two queries: select from emails where is_default = true, then select from emails where is_default = false. Then (default :: rest)).
+        language : string // (interface language for this user) - is this useful to the frontend? ... yeah, because when you log in, the front end wants to know who logged in and what language to give you
+    }
+
+    type UserList = UserDetails list
+
+    type MemberList = {
+        managers : string list  // Usernames
+        contributors : string list  // Usernames
+        observers : string list  // Usernames
+        programmers : string list  // Usernames
+    }
+
+    type ProjectDetails = {
+        code : string
+        name : string
+        description : string
+        // ``type`` : ProjectType  // TODO: Decide if we want this one or not
+        membership : MemberList option  // Because we'll sometimes want the membership list, and sometimes not. Only supplied if we asked for it in the request API, otherwise it's None/omitted
+    }
+
+    type ProjectList = ProjectDetails list  // Depending on the situation, this will sometimes include MemberLists and sometimes it won't (e.g., list all projects vs. list one's I'm a member of)
+
+    type RoleDetails = {
+        name : string
+        ``type`` : RoleType
+    }
+
+    let standardRoles : RoleDetails list = [
+        { name = "Manager"
+          ``type`` = Manager }
+        { name = "Contributor"
+          ``type`` = Contributor }
+        { name = "LanguageDepotProgrammer"
+          ``type`` = Programmer }
+        { name = "Observer"
+          ``type`` = Observer }
+    ]
+    // Hard-coded so we don't have to hit MySQL for something that hasn't changed in years.
+
+module Api =
+    type LoginCredentials = {
+        username : string
+        password : string
+    }
+
+    type CreateProject = {
+        login : LoginCredentials // TODO : Use "username" rather than "login" for login field in Redmine's users table
+        code : string
+        name : string
+        description : string option
+        initialMembers : Dto.MemberList option
+    }
+
+    type ArchiveProject = {
+        login : LoginCredentials
+        code : string
+    }
+
+    type DeleteProject = {
+        login : LoginCredentials
+        code : string
+    }
+
+    type MembershipRecordApiCall = {
+        username : string
+        role : string  // One of four values allowed : "manager", "contributor", "observer", "programmer" -- will be converted to MembershipType
+    }
+
+    type MembershipType =
+        | Manager
+        | Contributor
+        | Observer
+        | Programmer
+
+    type MembershipRecordInternal = {
+        username : string
+        role : MembershipType
+    }
+
+    type EditProjectMembershipApiCall = {
+        login : LoginCredentials // (the login credentials of an existing admin account, not the one being demoted)
+        projectCode : string
+        add : MembershipRecordApiCall list option
+        remove : MembershipRecordApiCall list option
+        removeUser : string option
+    }
+
+    type EditProjectMembershipInternalDetails =
+        | AddUserRoles of MembershipRecordInternal list
+        | RemoveUserRoles of MembershipRecordInternal list
+        | RemoveUserEntirely of string
+
+    type EditProjectMembershipInternal = {
+        login : LoginCredentials // (the login credentials of an existing admin account, not the one being demoted)
+        projectCode : string
+        details : EditProjectMembershipInternalDetails
+    }
+
+    type ChangeUserActiveStatus = {
+        login : LoginCredentials // (the login credentials of an existing admin account, or the one being changed)
+        username : string
+        active : bool  // Suspended = false, active = true
+    }
+
+    type DeleteUser = {
+        login : LoginCredentials // (the login credentials of an existing admin account, or the one being deleted)
+        username : string
+    }
+
+    type CreateUser = {
+        login : LoginCredentials // (the login credentials of the admin creating the account)
+        username : string // (required)
+        password : string // (required, cleartext - will be hashed by the server)
+        mustChangePassword : bool // (admin can choose for this to be false, but box is checked by default)
+        firstName : string
+        lastName : string
+        language : string option // (will default to "en" if not provided)
+        emailAddresses : string list // (may be empty. If not empty, first email in list will be default email and rest will not be default)
+    }
+
+    type EditUser = {
+        login : LoginCredentials // (the login credentials of the person editing the account : must either be same user as the account we're editing, or must be admin)
+        username : string // (required, cannot be changed in this API endpoint. A separate API endpoint exists for changing username)
+        // NOT password : string option (We'll use a separate API endpoint to change the password.)
+        firstName : string option
+        lastName : string option
+        language : string option // (will default to no change if not provided, just like every other string option in this model)
+        emailAddresses : string list option  // (if not provided, no change. If provided, replace current list with new one, even if new list is empty)
+    }
+
+    type ChangePassword = {
+        login : LoginCredentials // (the login credentials of the person changing the password : must either be same user as the account we're editing, or must be admin)
+        username : string
+        password : string
+        mustChangePassword : bool option // (option so it can be omitted : should be omitted/None if user is changing own password. If admin changing someone else's password, required.)
+    }
+
+    type ChangeUsername = {
+        login : LoginCredentials // (the login credentials of the person changing the username : must either be same user as the account we're editing, or must be admin)
+        oldUsername : string // (required even if we're logging in as that user, for API consistency)
+        newUsername : string
+    }
+
+    type PromoteUserToAdmin = {
+        login : LoginCredentials // (the login credentials of an existing admin account, not the one being promoted)
+        username : string
+    }
+
+    type DemoteAdminToNormalUser = {
+        login : LoginCredentials // (the login credentials of an existing admin account, not the one being demoted)
+        username : string
+    }
+
+    type ShowProjectsUserBelongsTo = {
+        login : LoginCredentials // (the login credentials of an admin account, or the user account whose username is specified below)
+        username : string
+        role : string option  // (if unspecified, return all projects user is a member of, else show projects where username holds this role)
+    }
+
+    type SearchProjects = {
+        login : LoginCredentials // (the login credentials of the user doing the search)
+        searchText : string option  // Search by either name or project code, but not(?) description
+        // If searchText is omitted, will return all projects ONLY if login account is admin. Otherwise will return all public projects.
+        offset : int option // If specified, used for paging
+        limit : int option  // If specified, used for paging
+    }
+
+    type SearchUsers = {
+        login : LoginCredentials // (the login credentials of the user doing the search)
+        emailSearch : string option // If specified, must be exact match (though case-insensitive (in invariant culture) so not "exact" by that standard)
+        nameSearch : string option // If specified, will do "LIKE '%str%'", so beware SQL injection here.
+        // If both email and name are specified, will do an AND. If an OR is desired, submit two API calls, one with emailSearch and one with nameSearch, and frontend can join them together while removing overlapping usernames
+        // If neither email nor name is specified, will return all users ONLY if login account is admin. Otherwise will return HTTP "Not Authorized" error.
+        offset : int option // If specified, used for paging
+        limit : int option  // If specified, used for paging
+    }
+
+
+// TODO: These two can probably be moved to Model.fs now
 module ProjectStatus =
     // Values copied from Redmine
     let [<Literal>] Active = 1
     let [<Literal>] Closed = 5
     let [<Literal>] Archived = 9
-
-// TODO: Decide whether all these fields in the Redmine SQL schema will actually be needed in our use case
-type Project = {
-    Id : int
-    Name : string
-    Description : string option // Long
-    Homepage : string option
-    IsPublic : bool // default true
-    ParentId : int option
-    CreatedOn : System.DateTime option
-    UpdatedOn : System.DateTime option
-    Identifier : string option // 20 chars
-    Status : int // default 1
-    Lft : int option
-    Rgt : int option
-    InheritMembers : bool // default false
-    DefaultVersionId : string option
-    DefaultAssignedToId : string option
-}
-// Lft and Rgt are part of Redmine's project hierarchy, which basically uses https://en.wikipedia.org/wiki/Nested_set_model to be able to do "is X a subproject of Y's hierarchy?" queries.
-// If a project has no subprojects, then its Rgt will be equal to its Lft + 1. If a situation arises where we can't set both of these to NULL, then a good value for a project with
-// no subprojects (and almost all projects created through this API will have no subprojects) is for Rgt to be equal to Id * 2, and Lft = Rgt - 1.
-
-type ProjectForListing = {
-    Id : int
-    Name : string
-    Typ : ProjectType
-    CreatedOn : System.DateTime option
-    Identifier : string option // 20 chars
-}
-
-// TODO: Expand and rename to ProjectDTO, or maybe ProjectCreationDTO if we need multiple kinds of DTO
-type CreateProject = { // Just a subset of fields
-    Name : string
-    Description : string option // Long
-    Identifier : string option // 20 chars
-}
 
 module UserStatus =
     // Values copied from Redmine
@@ -89,92 +260,4 @@ module UserStatus =
     let [<Literal>] Registered = 2
     let [<Literal>] Locked = 3
 
-type User = {
-    Id : int
-    Login : string
-    HashedPassword : string
-    FirstName : string
-    LastName : string
-    Admin : bool // default false
-    Status : int // default 1
-    LastLoginOn : System.DateTime option
-    Language : string option // 5 chars
-    AuthSourceId : int option
-    CreatedOn : System.DateTime option
-    UpdatedOn : System.DateTime option
-    Type : string option
-    IdentityUrl : string option
-    MailNotification : string
-    Salt : string option
-    MustChangePasswd : bool
-    PasswdChangedOn : System.DateTime option
-}
-
-(*
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `is_default` tinyint(1) NOT NULL DEFAULT '0',
-  `notify` tinyint(1) NOT NULL DEFAULT '1',
-  `created_on` datetime NOT NULL,
-  `updated_on` datetime NOT NULL,
-*)
-
-type MailAddress = {
-    Id : int
-    UserId : int
-    Address : string
-    IsDefault : bool
-    Notify : bool
-    CreatedOn : System.DateTime
-    UpdatedOn : System.DateTime
-}
-
-type CreateUser = { // Just a subset of fields
-    Login : string
-    CleartextPassword : string
-    FirstName : string
-    LastName : string
-    Mail : string
-}
-
-type UpdateUser = {
-    User : User
-    NewPassword : string option
-}
-
-type ChangePassword = {
-    OldPassword : string
-    NewPassword : string
-}
-
-type Role = {
-    Id : int
-    Name : string
-    Position : int option // Default 1
-    Assignable : bool
-    Builtin : int // Default 0
-    Permissions : string option // Long string containing newlines
-    IssuesVisibility : string // 30 chars, default "default"
-    UsersVisibility : string // 30 chars, default "all"
-    TimeEntriesVisibility : string // 30 chars, default "all"
-    AllRolesManaged : bool // default true
-    Settings : string option // Long string, default NULL
-}
-
-type Membership = {
-    Id : int
-    UserId : int // default 0
-    ProjectId : int // default 0
-    CreatedOn : System.DateTime option
-    MailNotification : bool // default false
-}
-
-type MembershipRole = {
-    Id : int
-    MembershipId : int // default 0
-    RoleId : int // default 0
-    InheritedFrom : int option
-}
-
-// TODO: Add a Repositories type so that our API can manage that table as well
+// TODO: Figure out an API for handling repositories
