63086f3017e8b186109630acd2f0bd9047d16ed9
Robin Munn <rmunn@pobox.com>
2019-12-12 10:22:29 +0700

    Fix ambiguous SQL in member removal API endpoint

--- src/Server/Model.fs
+++ src/Server/Model.fs
@@ -527,7 +527,7 @@ let removeMembership (connString : string) (username : string) (projectCode : st
         do! conn.OpenAsync()
         use transaction = conn.BeginTransaction()
         let sql =
-            "SELECT id FROM members" +
+            "SELECT members.id FROM members" +
             " JOIN users ON users.id = members.user_id" +
             " JOIN projects ON projects.id = members.project_id" +
             " WHERE users.login = @username AND projects.identifier = @projectCode"
