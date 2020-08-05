3366d52c583f490e2c38ff463c8ea9704a7197c4
Robin Munn <rmunn@pobox.com>
2020-08-05 16:27:53 +0700

    Better look for roles display table

--- src/Client/src/app/app.component.ts
+++ src/Client/src/app/app.component.ts
@@ -1,21 +1,20 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
+import { IdAndName } from './models/id-and-name.model';
 import { JsonApiService } from './services/json-api.service';
-import { retry } from 'rxjs/operators';
+import { retry, map } from 'rxjs/operators';
 import { PageEvent, MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'SafeNg';
 
-  numbersData: string[];
-  numbersLen: number;
   pageEvent: PageEvent;
-  dataSource: MatTableDataSource<string>;
+  dataSource: MatTableDataSource<IdAndName>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(private readonly jsonApi: JsonApiService ) {
@@ -40,7 +39,10 @@ export class AppComponent implements OnInit {
   roles(): void {
     const result = this.jsonApi.call<string[]>('/api/roles');
     result
-      .pipe(retry(3))
-      .subscribe(res => { this.dataSource.data = res; });
+      .pipe(
+        retry(3),
+        map(res => res.map(item => { return { id: item[0], name: item[1] } as IdAndName; }) )
+        )
+      .subscribe(res => { console.log("Got", res); this.dataSource.data = res; });
   }
 }
