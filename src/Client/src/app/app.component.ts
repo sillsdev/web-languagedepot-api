faba1d3469350a2612b4aebe11ba778bc9267914
Robin Munn <rmunn@pobox.com>
2020-08-06 17:42:40 +0700

    Now front page can load users, projects, and roles
    The data-table component I wrote is quite versatile. It *cannot* switch
    its table definition on the fly, though, so I need to use ngIf or
    ngSwitch to switch in or out the appropriate component for each query.
    But that's not going to be a problem in the actual app itself.

--- src/Client/src/app/app.component.ts
+++ src/Client/src/app/app.component.ts
@@ -6,6 +6,9 @@ import { PageEvent, MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { ColumnDescription } from './components/data-table.component';
+import { Observable } from 'rxjs';
+
+type Table = 'Users' | 'Projects' | 'Roles';
 
 @Component({
   selector: 'app-root',
@@ -16,37 +19,36 @@ export class AppComponent implements OnInit {
   title = 'SafeNg';
 
   pageEvent: PageEvent;
-  dataSource: MatTableDataSource<string[]>;
+  dataSource: MatTableDataSource<any>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  columnDescription: string[];
+  active: Table = 'Roles';
 
   constructor(private readonly jsonApi: JsonApiService ) {
     this.dataSource = new MatTableDataSource();
-    this.columnDescription = [ 'Num', 'Role' ];
   }
   ngOnInit(): void {
     this.dataSource.paginator = this.paginator;
   }
 
-  hello(): void {
-    const result = this.jsonApi.call<string>('/api/hello');
-    result.subscribe(res => console.log('Server-sent hello message was', res));
-  }
-  fail(): void {
-    const result = this.jsonApi.call<string>('/api/fail');
-    result.subscribe({
-      next: res => { console.log('Server-sent result was', res); },
-      error: err => { console.log('Server-sent error message was', err); },
-      complete: () => { console.log('fail() observable is complete'); }
-    });
-  }
-  roles(): void {
-    const result = this.jsonApi.call<string[][]>('/api/roles');
+  private populateData<T>(result: Observable<T[]>, whichTable: Table): void {
     result
       .pipe(
         retry(3),
         )
-      .subscribe(res => { console.log("Got", res); this.dataSource.data = res; });
+      .subscribe(res => { console.log('Got', res); this.active = whichTable; this.dataSource.data = res; });
+  }
+
+  users(): void {
+    const result = this.jsonApi.call<object[]>('/api/users');
+    this.populateData(result, 'Users');
+  }
+  projects(): void {
+    const result = this.jsonApi.call<object[]>('/api/project');
+    this.populateData(result, 'Projects');
+  }
+  roles(): void {
+    const result = this.jsonApi.call<string[][]>('/api/roles');
+    this.populateData(result, 'Roles');
   }
 }
