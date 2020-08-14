5d450a46737cee0610cbdf9005521b3e0b65f09f
Robin Munn <rmunn@pobox.com>
2020-08-14 11:50:02 +0700

    Remove unused import

--- src/Client/src/app/login/login.component.ts
+++ src/Client/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
