f59bfa3372617422b588ee383485c3347484f03b
Robin Munn <rmunn@pobox.com>
2021-01-27 08:38:05 +0700

    Consistent quote characters for JS strings

--- src/components/models/Role.js
+++ src/components/models/Role.js
@@ -1,7 +1,7 @@
 import { Model } from 'objection';
 
 class Role extends Model {
-    static get tableName() { return "roles"; }
+    static get tableName() { return 'roles'; }
 }
 
 export default Role;
