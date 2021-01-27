6b324542829a91f4f8197f1f434f819c2441b0b4
Robin Munn <rmunn@pobox.com>
2021-01-27 12:03:24 +0700

    Strip hashed_password from /api/users results

--- src/routes/api/users/index.js
+++ src/routes/api/users/index.js
@@ -1,11 +1,14 @@
-import User from '$components/models/User';
+import { User } from '$components/models/models';
 import { dbs } from '$components/models/dbsetup';
+import { withoutKey } from '$utils/withoutKey';
 
 export async function get() {
     try {
         const users = await User.query(dbs.private);
-        return { status: 200, body: users };
+        const result = users.map(withoutKey('hashed_password'));
+        return { status: 200, body: result };
     } catch (error) {
+        console.log(error);
         return { status: 500, body: { error, code: 'sql_error' } };
     }
 }
