8c00f817c1b21001701c1427700df806babd63b0
Robin Munn <rmunn@pobox.com>
2021-01-27 11:35:49 +0700

    Make /api/users into a directory
    This will allow us to write /api/users/[username]/projects and similar
    APIs later on.

--- src/routes/api/users/index.js
+++ src/routes/api/users/index.js
@@ -3,7 +3,7 @@ import { dbs } from '$components/models/dbsetup';
 
 export async function get() {
     try {
-        const users = await User.query(dbs.private).select('id', 'login');
+        const users = await User.query(dbs.private);
         return { status: 200, body: users };
     } catch (error) {
         return { status: 500, body: { error, code: 'sql_error' } };
