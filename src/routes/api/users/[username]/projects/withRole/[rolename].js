ea10d48ecd6c61ac38262381676c55323d02aac4
Robin Munn <rmunn@pobox.com>
2021-02-02 15:42:51 +0700

    Add /users/:username/projects/withRole/:rolename API route
    Searches for only projects where the user has a given role

---
+++ src/routes/api/users/[username]/projects/withRole/[rolename].js
@@ -0,0 +1,29 @@
+import { User } from '$components/models/models';
+import { dbs } from '$components/models/dbsetup';
+
+export async function get({ params, query }) {
+    if (!params.username) {
+        return { status: 500, body: { description: 'No username specified', code: 'missing_username' }};
+    }
+    if (!params.rolename) {
+        return { status: 500, body: { description: 'No rolename specified', code: 'missing_rolename' }};
+    }
+    const db = query.private ? dbs.private : dbs.public;
+    try {
+        const users = await User.query(db)
+            .withGraphJoined('memberships.[project, role]')
+            .where('login', params.username)
+            .andWhere('memberships:role.name', params.rolename);
+        const result = users.map(user => ({
+            username: user.login,
+            memberships: user.memberships.map(m => ({
+                projectCode: m.project.identifier,
+                role: m.role.name,
+            }))
+        }));
+        return { status: 200, body: result };
+    } catch (error) {
+        console.log('Error of type', typeof error, 'has name:', error.name, 'and msg:', error.message);
+        return { status: 500, body: { error, code: 'sql_error' } };
+    }
+}
