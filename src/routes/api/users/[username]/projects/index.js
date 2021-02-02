9d488aab5530101980af0468678b5e75b3ae43e9
Robin Munn <rmunn@pobox.com>
2021-02-02 16:06:37 +0700

    Check auth in "get projects for user" routes
    For these two routes, which may be accessed by older clients, we allow
    both JWT and HTTP basic auth methods to specify the username (and
    password, if basic auth) of the user whose projects are requested.

--- src/routes/api/users/[username]/projects/index.js
+++ src/routes/api/users/[username]/projects/index.js
@@ -1,10 +1,25 @@
 import { dbs } from '$components/models/dbsetup';
+import { verifyJwtAuth } from '$utils/db/auth';
+import { allowSameUserOrAdmin } from '$utils/db/authRules';
 import { getProjectsForUser } from '$utils/db/usersAndRoles';
 
-export async function get({ params, path, query }) {
+export async function get({ params, path, query, headers }) {
     if (!params.username) {
         return missingRequiredParam('username', path);
     }
     const db = query.private ? dbs.private : dbs.public;
-    return getProjectsForUser(db, params);
+    let authUser = await verifyJwtAuth(db, headers);
+    if (!authUser) {
+        // To interop with older clients, this route also allows user:pass in URL (HTTP basic auth)
+        authUser = await verifyBasicAuth(db, headers);
+        if (!authUser) {
+            return authTokenRequired();
+        }
+    }
+    const authResponse = allowSameUserOrAdmin({ params, authUser });
+    if (authResponse.status === 200) {
+        return getProjectsForUser(db, params);
+    } else {
+        return authResponse;
+    }
 }
--- src/routes/api/users/[username]/projects/withRole/[rolename].js
+++ src/routes/api/users/[username]/projects/withRole/[rolename].js
@@ -1,8 +1,10 @@
 import { dbs } from '$components/models/dbsetup';
-import { missingRequiredParam } from '$utils/commonErrors';
+import { authTokenRequired, missingRequiredParam } from '$utils/commonErrors';
+import { verifyBasicAuth, verifyJwtAuth } from '$utils/db/auth';
+import { allowSameUserOrAdmin } from '$utils/db/authRules';
 import { getProjectsForUser } from '$utils/db/usersAndRoles';
 
-export async function get({ params, path, query }) {
+export async function get({ params, path, query, headers }) {
     if (!params.username) {
         return missingRequiredParam('username', path);
     }
@@ -10,5 +12,18 @@ export async function get({ params, path, query }) {
         return missingRequiredParam('rolename', path);
     }
     const db = query.private ? dbs.private : dbs.public;
-    return getProjectsForUser(db, params);
+    let authUser = await verifyJwtAuth(db, headers);
+    if (!authUser) {
+        // To interop with older clients, this route also allows user:pass in URL (HTTP basic auth)
+        authUser = await verifyBasicAuth(db, headers);
+        if (!authUser) {
+            return authTokenRequired();
+        }
+    }
+    const authResponse = allowSameUserOrAdmin({ params, authUser });
+    if (authResponse.status === 200) {
+        return getProjectsForUser(db, params);
+    } else {
+        return authResponse;
+    }
 }
