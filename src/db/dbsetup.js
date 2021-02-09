0f8f45e578726f9ed070e7eab440c029c5e04b9f
Robin Munn <rmunn@pobox.com>
2021-02-09 11:51:42 +0700

    Use env name MYSQL_DATABASE instead of MYSQL_DB
    MYSQL_DATABASE matches the environment variable name expected by the
    Docker MySQL image (https://hub.docker.com/_/mysql/) which will make it
    easier to set up deployments, as I can just set up a secret with
    MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD and so on, and allow that
    secret to populate environment variables for both the mysql container
    and the ldapi container.

--- src/db/dbsetup.js
+++ src/db/dbsetup.js
@@ -14,7 +14,7 @@ const dbs = {
                 host : process.env.MYSQL_HOST || '127.0.0.1',
                 user : process.env.MYSQL_USER || '',
                 password : process.env.MYSQL_PASSWORD || '',
-                database : process.env.MYSQL_DB || 'testldapi'
+                database : process.env.MYSQL_DATABASE || 'testldapi'
             },
         });
     },
@@ -29,7 +29,7 @@ const dbs = {
                 host : process.env.MYSQL_HOST || '127.0.0.1',
                 user : process.env.MYSQL_USER || '',
                 password : process.env.MYSQL_PASSWORD || '',
-                database : process.env.MYSQL_DB || 'testldapi' // `${process.env.MYSQL_DB || 'testldapi'}pvt`
+                database : process.env.MYSQL_DATABASE || 'testldapi' // `${process.env.MYSQL_DATABASE || 'testldapi'}pvt`
             },
         });
     }
