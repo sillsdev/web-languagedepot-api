ce51e7d4a2118efd0491531c10853dc25f6e84bf
Robin Munn <rmunn@pobox.com>
2021-04-21 14:54:10 +0700

    Add test data setup script for MySQL and Postgres
    Also add a shell script to run either test data setup script, depending
    on whether POSTGRES_HOST is set (if not, it defaults to MySQL).

---
+++ test/restoretestdata.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+yesno() {
+    read -p "Please press 'y' to continue, anything else to quit: " -n 1
+    echo
+    case "$REPLY" in
+        [Yy]* ) true;;
+        * ) false;;
+    esac
+}
+
+if [ "$POSTGRES_HOST" ];
+then
+    # Set defaults if not set
+    : ${POSTGRES_DATABASE:=testldapi}
+    : ${POSTGRES_PORT=5432}
+    echo "Will load test data into Postgres DB ${POSTGRES_DATABASE} on ${POSTGRES_HOST}:${POSTGRES_PORT}"
+    yesno || exit
+    PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" $POSTGRES_DATABASE < pg-testlanguagedepot.sql
+else
+    # Set defaults if not set
+    : ${MYSQL_HOST=localhost}
+    : ${MYSQL_DATABASE:=testldapi}
+    : ${MYSQL_PORT=5432}
+    echo "Will load test data into MySQL DB ${MYSQL_DATABASE} on ${MYSQL_HOST}:${MYSQL_PORT}"
+    yesno || exit
+    mysql -u "$MYSQL_USER" --password="$MYSQL_PASSWORD" -h "$MYSQL_HOST" -P "$MYSQL_PORT" $MYSQL_DATABASE < testlanguagedepot.sql
+fi
